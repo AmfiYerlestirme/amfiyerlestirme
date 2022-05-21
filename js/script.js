2cbf3e199979b7cbc189f3eeaea3fbe03e084002
Anonymous <anonymous>
2022-05-21 15:12:20 +0300

    Add legend

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ $(window).on("load", function () {
         });
     });
     $("#download_button").click(function () {
+        appendLegend(svgDoc);
         const svgData = new XMLSerializer().serializeToString(svgDoc);
         const canvas = document.createElement("canvas");
         canvas.width = 1920;
@@ -51,4 +52,47 @@ $(window).on("load", function () {
             };
         });
     });
+    function appendLegend(svgDoc) {
+        const gTag = svgDoc.getElementsByTagName("g")[0];
+        const legendRectA = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        legendRectA.setAttribute("style", "fill:#ff0000;stroke-width:1.72032");
+        legendRectA.setAttribute("id", "legend_rect_a");
+        legendRectA.setAttribute("width", "30");
+        legendRectA.setAttribute("height", "30");
+        legendRectA.setAttribute("x", "1754.4463");
+        legendRectA.setAttribute("y", "961.03424");
+
+        const legendRectB = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+        legendRectB.setAttribute("style", "fill:#0000ff;stroke-width:1.72032");
+        legendRectB.setAttribute("id", "legend_rect_b");
+        legendRectB.setAttribute("width", "30");
+        legendRectB.setAttribute("height", "30");
+        legendRectB.setAttribute("x", "1754.4463");
+        legendRectB.setAttribute("y", "991.03424");
+
+        const legendTextA = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+        legendTextA.setAttribute("xml:space", "preserve");
+        legendTextA.setAttribute("style", "font-style:normal;font-weight:normal;font-size:26.6667px;line-height:1.25;font-family:sans-serif;fill:#000000;fill-opacity:1;stroke:none");
+        legendTextA.setAttribute("id", "legend_text_a");
+        legendTextA.setAttribute("x", "1788.4463");
+        legendTextA.setAttribute("y", "985.03424");
+
+        var textNodeA = document.createTextNode("A");
+        legendTextA.appendChild(textNodeA);
+
+        const legendTextB = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+        legendTextB.setAttribute("xml:space", "preserve");
+        legendTextB.setAttribute("style", "font-style:normal;font-weight:normal;font-size:26.6667px;line-height:1.25;font-family:sans-serif;fill:#000000;fill-opacity:1;stroke:none");
+        legendTextB.setAttribute("id", "legend_text_b");
+        legendTextB.setAttribute("x", "1788.4463");
+        legendTextB.setAttribute("y", "1015.0342");
+
+        var textNodeB = document.createTextNode("B");
+        legendTextB.appendChild(textNodeB);
+
+        gTag.appendChild(legendRectA);
+        gTag.appendChild(legendRectB);
+        gTag.appendChild(legendTextA);
+        gTag.appendChild(legendTextB);
+    }
 });
